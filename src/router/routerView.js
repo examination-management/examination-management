88e6ccfa2b2fd1ab0713f08fe788fae45de85cb1
meimei <1254630731@qq.com>
2019-09-07 16:53:03 +0800

    完整

--- src/router/routerView.js
+++ src/router/routerView.js
@@ -1,31 +1,17 @@
-<<<<<<< HEAD
-import React, { Component } from "react";
-import { Switch, Route, Redirect, BrowserRouter } from "react-router-dom";
-export default class routerView extends Component {
-=======
 import * as React from "react";
 import { Switch, Route, Redirect, BrowserRouter } from "react-router-dom";
 export default class RouterView extends React.Component{
->>>>>>> master
   constructor(props) {
     super(props);
   }
   render() {
     let { routes } = this.props;
-<<<<<<< HEAD
-    let routerArr = routes.filter(item => !item.redirect);
-    let redirectArr = routes.filter(item => item.redirect);
-    return (
-      <Switch>
-        {routerArr.map((item, index) => (
-=======
     console.log(routes,"view")
     let routerArr = routes?routes.filter(item => !item.redirect):"";
     let redirectArr =  routes?routes.filter(item => item.redirect):"";
     return (
       <Switch>
         {routerArr?routerArr.map((item, index) => (
->>>>>>> master
           <Route
             path={item.path}
             key={index}
@@ -38,17 +24,10 @@ export default class RouterView extends React.Component{
               );
             }}
           ></Route>
-<<<<<<< HEAD
-        ))}
-        {redirectArr.map((item, index) => (
-          <Redirect from={item.path} key={index} to={item.redirect}></Redirect>
-        ))}
-=======
         )):""}
         {redirectArr?redirectArr.map((item, index) => (
           <Redirect from={item.path} key={index} to={item.redirect}></Redirect>
         )):""}
->>>>>>> master
       </Switch>
     );
   }
